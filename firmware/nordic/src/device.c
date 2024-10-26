b0d54a432d38a9ffc633b5f05bf5385efde22eec
Ben Owen <ben@bensuniverse.com>
2024-10-26 12:23:53 -0400

    Rename sys function calls to device

--- firmware/nordic/src/device.c
+++ firmware/nordic/src/device.c
@@ -1,8 +1,8 @@
 /**
- * @file sys.c
+ * @file device.c
  * @author Ben Owen (ben@embeddedshark.com)
- * @brief File containing global system-level functions
- *          Also contains global system-level state variables
+ * @brief File containing global device-level functions
+ *          Also contains global device-level state variables
  * 
  * @date 2024-10-21
  * 
@@ -10,10 +10,12 @@
  * 
  */
 
-#include "sys.h"
+#include "device.h"
 
 #include "button.h"
 #include "led.h"
+
+#include <drivers/lis2dtw12.h>
 
 #include <nrfx_gpiote.h>
 #include <nrfx_timer.h>
@@ -23,25 +25,46 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/poweroff.h>
 
-LOG_MODULE_REGISTER(SYS, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(DEVICE, LOG_LEVEL_INF);
 
 /**
  * LOCAL VARIABLES
  */
 
-sys_state_t sys_state = SYS_STATE_POWEROFF;
+static device_state_t _device_state;
 
 /**
  * FUNCTION DEFINITIONS
  */
 
-void sys_power_off(const struct gpio_dt_spec * button_dt)
+device_state_t device_get_state(void)
+{
+    return _device_state;
+}
+
+void device_set_state(device_state_t new_state)
+{
+    _device_state = new_state;
+}
+
+void device_wakeup(void)
+{
+    /* Set boot LED pattern */
+    led_set_pattern(LED_PATTERN_PULSE);
+    /* Set devicetem state */
+    device_set_state(DEVICE_STATE_WAKEUP);
+}
+
+void device_poweroff(const struct gpio_dt_spec * button_dt)
 {
     nrfx_err_t err;
 
-    // TODO: Turn off accelerometer - setting CTRL1 to 0x0000 will put device into power-down mode
+    /* Change device state */
+    device_set_state(DEVICE_STATE_POWEROFF);
     /* Clear LED */
     led_set_pattern(LED_PATTERN_OFF);
+    /* Turn off accelerometer */
+    lis2dtw12_power_off();  // TODO not working since called from interrupt function
     /* Wait until button released */
     while (gpio_pin_get_dt(button_dt));
     k_msleep(250);  // delay a small period for button bouncing
@@ -49,11 +72,11 @@ void sys_power_off(const struct gpio_dt_spec * button_dt)
     err = gpio_pin_interrupt_configure_dt(button_dt, GPIO_INT_LEVEL_ACTIVE);
     NRFX_ASSERT(err == 0);
     /* Put microcontroller to sleep */
-    LOG_WRN("Powering system off");
+    LOG_WRN("Powering devicetem off");
     sys_poweroff();
 }
 
-sys_reset_src_t sys_get_reset_src(void)
+device_reset_src_t device_get_reset_src(void)
 {
     /* Get reset source */
     uint32_t reset_src = NRF_POWER->RESETREAS;
@@ -63,26 +86,26 @@ sys_reset_src_t sys_get_reset_src(void)
     /* Determine reset reason */
     if (reset_src & POWER_RESETREAS_OFF_Msk)
     {
-        return SYS_RESET_SRC_GPIO_WAKEUP;
+        return DEVICE_RESET_SRC_GPIO_WAKEUP;
     }
     else if (reset_src & POWER_RESETREAS_LOCKUP_Msk)
     {
-        return SYS_RESET_SRC_CPU_LOCKUP;
+        return DEVICE_RESET_SRC_CPU_LOCKUP;
     }
     else if (reset_src & POWER_RESETREAS_SREQ_Msk)
     {
-        return SYS_RESET_SRC_SOFT_RESET;
+        return DEVICE_RESET_SRC_SOFT_RESET;
     }
     else if (reset_src & POWER_RESETREAS_DOG_Msk)
     {
-        return SYS_RESET_SRC_WATCHDOG;
+        return DEVICE_RESET_SRC_WATCHDOG;
     }
     else if (reset_src & POWER_RESETREAS_RESETPIN_Msk)
     {
-        return SYS_RESET_SRC_RESET_PIN;
+        return DEVICE_RESET_SRC_RESET_PIN;
     }
     else
     {
-        return SYS_RESET_SRC_OTHER;
+        return DEVICE_RESET_SRC_OTHER;
     }
 }
---
+++ firmware/nordic/src/device.h
@@ -0,0 +1,76 @@
+/**
+ * @file device.h
+ * @author Ben Owen (ben@embeddedshark.com)
+ * @brief Header file for device.c
+ * 
+ * @date 2024-10-21
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef __DEVICE_H__
+#define __DEVICE_H__
+
+#include <zephyr/drivers/gpio.h>
+
+/* ENUMERATION DEFINITIONS */
+
+typedef enum
+{
+    DEVICE_STATE_POWEROFF,
+    DEVICE_STATE_WAKEUP,
+    DEVICE_STATE_RUN,
+}
+device_state_t;
+
+typedef enum
+{
+    DEVICE_RESET_SRC_GPIO_WAKEUP,
+    DEVICE_RESET_SRC_CPU_LOCKUP,
+    DEVICE_RESET_SRC_SOFT_RESET,
+    DEVICE_RESET_SRC_WATCHDOG,
+    DEVICE_RESET_SRC_RESET_PIN,
+    DEVICE_RESET_SRC_OTHER,
+}
+device_reset_src_t;
+
+/**
+ * FUNCTION DECLARATIONS
+ */
+
+/**
+ * @brief Get the device state
+ * 
+ */
+device_state_t device_get_state(void);
+
+/**
+ * @brief Change the device state
+ * 
+ * @param new_state 
+ */
+void device_set_state(device_state_t new_state);
+
+/**
+ * @brief Device initialization when waking from sleep
+ * 
+ */
+void device_wakeup(void);
+
+/**
+ * @brief Turn the device off
+ *          Handles cleanup before shutting off
+ * 
+ * @param button_dt 
+ */
+void device_poweroff(const struct gpio_dt_spec * button_dt);
+
+/**
+ * @brief Get the device wakeup reason
+ * 
+ * @return device_reset_src_t 
+ */
+device_reset_src_t device_get_reset_src(void);
+
+#endif  /* __DEVICE_H__ */
